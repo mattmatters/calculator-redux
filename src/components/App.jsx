aa9e00505c6627e4f125ae48eb9f21d0232a8272
Matt Lewis <domattthings@gmail.com>
2017-01-11 00:32:14 +0000

    Removed unnessessary eslint comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Display from '../containers/displayContainer';
 import ButtonPad from '../containers/buttonPadContainer';
 
-// eslint-disable-next-line
 const App = () => (
   <div className="app container">
     <h1 className="text-center"> Calculator-Redux </h1>
